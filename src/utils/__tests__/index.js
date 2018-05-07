2a8e86c7dbe3370ba41986e5c8407174a7bdff85
Jason Yu <me@ycmjason.com>
2018-05-07 21:09:42 +0100

    added matcher for whole number

--- src/utils/__tests__/index.js
+++ src/utils/__tests__/index.js
@@ -1,5 +1,21 @@
 import { $random } from '../index';
 
+expect.extend({
+  toBeWholeNumber(received) {
+    if (received % 1 === 0) {
+      return {
+        message: () => `expected ${received} not to be a whole number`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () => `expected ${received} to be a whole number`,
+        pass: false,
+      };
+    }
+  },
+});
+
 describe('utils', () => {
   describe('$random', () => {
     it('should generate random from 0 to 1 by default', () => {
@@ -19,8 +35,7 @@ describe('utils', () => {
     describe('$random.rounded', () => {
       it('should round the number', () => {
         Array.from({ length: 1000 }, () => $random.rounded(50, 100)).forEach(x => {
-          // make sure it is whole number
-          expect(x % 1).toBe(0);
+          expect(x).toBeWholeNumber();
         });
       });
 
