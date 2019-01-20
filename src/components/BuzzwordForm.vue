5766a7c7d941c6f7deb477a7f1c63253391c0e14
Jason Yu <me@ycmjason.com>
2019-01-20 10:57:46 +0000

    refactor: removed TextInput and make the BuzzwordForm less complicated

--- src/components/BuzzwordForm.vue
+++ src/components/BuzzwordForm.vue
@@ -1,13 +1,15 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <mj-text-input
-      class="buzzwordInput"
-      placeholder="Game Buzzword"
-      text-transform="uppercase"
-      :is-invalid-key="c => /^[^a-zA-Z]$/.test(c)"
-      :valid-pattern="/^[A-Z]+$/"
-      v-model="buzzword" />
+    <input class="buzzwordInput"
+           :class="invalidClass"
+           placeholder="Game Buzzword"
+           pattern="^[A-Za-z0-9]+$"
+           @keydown="onKeydown"
+           v-model="buzzword" >
     <input class="startButton" type="submit" value="Start">
+    <p class="patternHint" :class="invalidClass">
+      Hint: You can only use a combination of capital letters and numbers.
+    </p>
   </form>
 </template>
 
@@ -21,18 +23,45 @@ export default {
       this.buzzword = buzzword;
     });
   },
+
   data: () => ({
     buzzword: '',
     isPristine: true,
   }),
 
+  watch: {
+    buzzword () {
+      this.isPristine = false;
+    },
+  },
+
+  computed: {
+    upperBuzzword () {
+      return this.buzzword.toUpperCase();
+    },
+
+    isValid () {
+      return /^[A-Z0-9]+$/.test(this.upperBuzzword);
+    },
+
+    invalidClass () {
+      return {
+        invalid: !this.isPristine && !this.isValid,
+      };
+    },
+  },
+
   methods: {
     onSubmit () {
-      const { buzzword } = this;
+      const { upperBuzzword, isValid } = this;
 
-      this.isPristine = false;
-      if (buzzword.length > 0) {
-        this.$emit('submit', buzzword);
+      if (isValid) {
+        this.$emit('submit', upperBuzzword);
+      }
+    },
+    onKeydown ($event) {
+      if (!/[A-Za-z0-9]/.test($event.key)) {
+        $event.preventDefault();
       }
     },
   },
@@ -48,12 +77,17 @@ form {
 }
 
 .buzzwordInput {
+  text-transform: uppercase;
   flex-basis: 100%;
   flex-shrink: 0;
 
   @media screen and (min-width: $breakpoint-sm) {
     flex-basis: 0;
     flex-grow: 10;
+  }
+
+  &.invalid {
+    border-color: $color-danger;
   }
 }
 
@@ -64,5 +98,14 @@ form {
 
 .errorMessage {
   flex-basis: 100%;
+}
+
+.patternHint {
+  display: none;
+
+  &.invalid {
+    display: block;
+    color: $color-danger;
+  }
 }
 </style>
